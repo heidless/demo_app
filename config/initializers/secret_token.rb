d99e5497206c2f5d3362cdb154bceb184c2357df
Rob Craig on Cuba <robcraig@wordtracker.com>
2013-01-27 14:18:35 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '27520ee4b2d85b5637bf585c2ded087b3ad1242381fb335049b3f20802da9b6921de387bcdc10e6357135316ec27f56ab60bf22d2bc16bfddf964331d01717ca'
